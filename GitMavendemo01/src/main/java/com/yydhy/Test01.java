ba2499d69997560399c0c67ebc35234c862c440e
Administrator <Administrator@tqm>
2016-07-07 07:58:49 +0800

    第一次版本提交，功能不完善

---
+++ GitMavendemo01/src/main/java/com/yydhy/Test01.java
@@ -0,0 +1,16 @@
+package com.yydhy;
+
+public class Test01 {
+	public static void main(String[] args) {
+		System.err.println("git≤‚ ‘");
+		System.err.println("git≤‚ ‘");
+		System.err.println("git≤‚ ‘");
+		System.err.println("git≤‚ ‘");
+		System.err.println("git≤‚ ‘");
+		System.err.println("git≤‚ ‘");
+		System.err.println("git≤‚ ‘");
+		System.err.println("git≤‚ ‘");
+		System.err.println("git≤‚ ‘");
+	}
+
+}
