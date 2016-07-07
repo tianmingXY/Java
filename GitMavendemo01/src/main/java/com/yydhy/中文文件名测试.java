f497876d81703da3a739a00fb99756e0afb9f826
Administrator <Administrator@tqm>
2016-07-07 11:56:06 +0800

    中文文件名测试

---
+++ GitMavendemo01/src/main/java/com/yydhy/中文文件名测试.java
@@ -0,0 +1,13 @@
+package com.yydhy;
+
+/**
+ * java git 测试测试
+ * @author Administrator
+ *
+ */
+public class 中文文件名测试 {
+	public static void main(String[] args) {
+		System.err.println("测试git版本控制管理");
+	}
+
+}
